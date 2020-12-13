cd95f31005a66538709744761ef3e0f8794d427e
Julio Gutierrez <bubuntux@gmail.com>
2020-12-13 02:58:05 -0600

    -fix nordvpn 3.7.4 version -base from ubuntu lts -change travis for github actions

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,24 @@
-ARG ARCH=amd64
-FROM balenalib/${ARCH}-debian
+FROM ubuntu:18.04
 
-ARG NORDVPN_VERSION
 LABEL maintainer="Julio Gutierrez"
+ARG NORDVPN_VERSION=3.7.4
 
 HEALTHCHECK --interval=5m --timeout=20s --start-period=1m \
-  CMD if test $( curl -m 10 -s https://api.nordvpn.com/vpn/check/full | jq -r '.["status"]' ) = "Protected" ; then exit 0; else nordvpn connect ${CONNECT} ; exit $?; fi
+	CMD if test $( curl -m 10 -s https://api.nordvpn.com/vpn/check/full | jq -r '.["status"]' ) = "Protected" ; then exit 0; else nordvpn connect ${CONNECT} ; exit $?; fi
 
-#CROSSRUN [ "cross-build-start" ]
 RUN addgroup --system vpn && \
-    apt-get update && apt-get upgrade -y && \
-    apt-get install -y wget dpkg curl gnupg2 jq traceroute && \
-    wget -nc https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb && dpkg -i nordvpn-release_1.0.0_all.deb && \
-    apt-get update && apt-get install -yqq nordvpn${NORDVPN_VERSION:+=$NORDVPN_VERSION} || sed -i "s/init)/$(ps --no-headers -o comm 1))/" /var/lib/dpkg/info/nordvpn.postinst && \
-    update-alternatives --set iptables /usr/sbin/iptables-legacy && \
-    update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy && \
-    apt-get install -yqq && apt-get clean && \
+	apt update && \
+	apt install -y curl jq && \
+	curl https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb --output /tmp/nordrepo.deb && \
+    apt install -y /tmp/nordrepo.deb && \
+    apt update && \
+    apt install -y nordvpn${NORDVPN_VERSION:+=$NORDVPN_VERSION} && \
+    apt remove -y nordvpn-release && \
     rm -rf \
-        ./nordvpn* \
-        /tmp/* \
-        /var/lib/apt/lists/* \
-        /var/tmp/*
-#CROSSRUN [ "cross-build-end" ]
+		/tmp/* \
+		/var/cache/apt/archives/* \
+		/var/lib/apt/lists/* \
+		/var/tmp/*
 
 CMD /usr/bin/start_vpn.sh
 COPY start_vpn.sh /usr/bin
-
--- start_vpn.sh
+++ start_vpn.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 [[ -n ${DEBUG} ]] && set -x
 [[ -n ${COUNTRY} && -z ${CONNECT} ]] && CONNECT=${COUNTRY}
-[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && groupmod -g $GROUPID -o vpn
+[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && groupmod -g "$GROUPID" -o vpn
 
 DOCKER_NET="$(ip -o addr show dev eth0 | awk '$3 == "inet" {print $4}')" 
 
 kill_switch() {
-	local  docker6_network="$(ip -o addr show dev eth0 | awk '$3 == "inet6" {print $4; exit}')"	
-
 	iptables -F
 	iptables -X
 	iptables -P INPUT DROP
@@ -31,14 +29,15 @@ kill_switch() {
 	}
 	iptables -t nat -A POSTROUTING -o tap+ -j MASQUERADE
 	iptables -t nat -A POSTROUTING -o tun+ -j MASQUERADE
+	iptables -t nat -A POSTROUTING -o nordlynx -j MASQUERADE
 	if [[ -n ${DOCKER_NET} ]]; then
-		iptables -A INPUT -s ${DOCKER_NET} -j ACCEPT
-		iptables -A FORWARD -d ${DOCKER_NET} -j ACCEPT
-		iptables -A FORWARD -s ${DOCKER_NET} -j ACCEPT
-		iptables -A OUTPUT -d ${DOCKER_NET} -j ACCEPT
+		iptables -A INPUT -s "${DOCKER_NET}" -j ACCEPT
+		iptables -A FORWARD -d "${DOCKER_NET}" -j ACCEPT
+		iptables -A FORWARD -s "${DOCKER_NET}" -j ACCEPT
+		iptables -A OUTPUT -d "${DOCKER_NET}" -j ACCEPT
 	fi
-	[[ -n ${NETWORK} ]]  && for net in ${NETWORK//[;,]/ };  do return_route ${net};  done
-	[[ -n ${WHITELIST} ]] && for domain in ${WHITELIST//[;,]/ }; do white_list ${domain}; done
+	[[ -n ${NETWORK} ]]  && for net in ${NETWORK//[;,]/ };  do return_route "${net}";  done
+	[[ -n ${WHITELIST} ]] && for domain in ${WHITELIST//[;,]/ }; do white_list "${domain}"; done
 
 	ip6tables -F 2>/dev/null
 	ip6tables -X 2>/dev/null
@@ -63,35 +62,37 @@ kill_switch() {
 		ip6tables -A OUTPUT -p tcp -m tcp --dport 443   -j ACCEPT 2>/dev/null
 		ip6tables -A OUTPUT -o eth0 -d api.nordvpn.com  -j ACCEPT 2>/dev/null
 	}
+
+  local docker6_network="$(ip -o addr show dev eth0 | awk '$3 == "inet6" {print $4; exit}')"
 	if [[ -n ${docker6_network} ]]; then
-		ip6tables -A INPUT -s ${docker6_network} -j ACCEPT 2>/dev/null
-		ip6tables -A FORWARD -d ${docker6_network} -j ACCEPT 2>/dev/null
-		ip6tables -A FORWARD -s ${docker6_network} -j ACCEPT 2>/dev/null
-		ip6tables -A OUTPUT -d ${docker6_network} -j ACCEPT 2>/dev/null
+		ip6tables -A INPUT -s "${docker6_network}" -j ACCEPT 2>/dev/null
+		ip6tables -A FORWARD -d "${docker6_network}" -j ACCEPT 2>/dev/null
+		ip6tables -A FORWARD -s "${docker6_network}" -j ACCEPT 2>/dev/null
+		ip6tables -A OUTPUT -d "${docker6_network}" -j ACCEPT 2>/dev/null
 	fi
-	[[ -n ${NETWORK6} ]] && for net in ${NETWORK6//[;,]/ }; do return_route6 ${net}; done
+	[[ -n ${NETWORK6} ]] && for net in ${NETWORK6//[;,]/ }; do return_route6 "${net}"; done
 }
 
 return_route() { # Add a route back to your network, so that return traffic works
-	local network="$1" gw="$(ip route |awk '/default/ {print $3}')"
-    	ip route | grep -q "$network" || ip route add to $network via $gw dev eth0
-    	iptables -A INPUT -s $network -j ACCEPT
-    	iptables -A FORWARD -d $network -j ACCEPT
-    	iptables -A FORWARD -s $network -j ACCEPT
-    	iptables -A OUTPUT -d $network -j ACCEPT
+	local network="$1" gw=$(ip route |awk '/default/ {print $3}')
+    	ip route | grep -q "$network" || ip route add to "$network" via "$gw" dev eth0
+    	iptables -A INPUT -s "$network" -j ACCEPT
+    	iptables -A FORWARD -d "$network" -j ACCEPT
+    	iptables -A FORWARD -s "$network" -j ACCEPT
+    	iptables -A OUTPUT -d "$network" -j ACCEPT
 }
 
 return_route6() { # Add a route back to your network, so that return traffic works
-	local network="$1" gw="$(ip -6 route | awk '/default/{print $3}')"
-	ip -6 route | grep -q "$network" || ip -6 route add to $network via $gw dev eth0
-	ip6tables -A INPUT -s $network -j ACCEPT 2>/dev/null
-	ip6tables -A FORWARD -d $network -j ACCEPT 2>/dev/null
-	ip6tables -A FORWARD -s $network -j ACCEPT 2>/dev/null
-	ip6tables -A OUTPUT -d $network -j ACCEPT 2>/dev/null
+	local network="$1" gw=$(ip -6 route | awk '/default/{print $3}')
+	ip -6 route | grep -q "$network" || ip -6 route add to "$network" via "$gw" dev eth0
+	ip6tables -A INPUT -s "$network" -j ACCEPT 2>/dev/null
+	ip6tables -A FORWARD -d "$network" -j ACCEPT 2>/dev/null
+	ip6tables -A FORWARD -s "$network" -j ACCEPT 2>/dev/null
+	ip6tables -A OUTPUT -d "$network" -j ACCEPT 2>/dev/null
 }
 
 white_list() { # Allow unsecured traffic for an specific domain
-	local domain=`echo $1 | sed 's/^.*:\/\///;s/\/.*$//'`
+	local domain=$(echo "$1" | sed 's/^.*:\/\///;s/\/.*$//')
 	sg vpn -c "iptables  -A OUTPUT -o eth0 -d ${domain} -j ACCEPT"
 	sg vpn -c "ip6tables -A OUTPUT -o eth0 -d ${domain} -j ACCEPT 2>/dev/null"
 }
@@ -108,22 +109,22 @@ setup_nordvpn() {
 	[[ -n ${CYBER_SEC} ]] && nordvpn set cybersec ${CYBER_SEC}
 	[[ -n ${DNS} ]] && nordvpn set dns ${DNS//[;,]/ }
 	[[ -n ${DOCKER_NET} ]]  && nordvpn whitelist add subnet ${DOCKER_NET}
-	[[ -n ${NETWORK} ]]  && for net in ${NETWORK//[;,]/ };  do nordvpn whitelist add subnet ${net};  done
-	[[ -n ${PORTS} ]]  && for port in ${PORTS//[;,]/ };  do nordvpn whitelist add port ${port};  done
+	[[ -n ${NETWORK} ]]  && for net in ${NETWORK//[;,]/ };  do nordvpn whitelist add subnet "${net}";  done
+	[[ -n ${PORTS} ]]  && for port in ${PORTS//[;,]/ };  do nordvpn whitelist add port "${port}";  done
 	[[ -n ${DEBUG} ]] && nordvpn -version && nordvpn settings
 }
 
 kill_switch
 
-pkill nordvpnd 
+pkill nordvpnd
 rm -f /run/nordvpnd.sock
-sg vpn -c nordvpnd & 
+sg vpn -c nordvpnd &
 
 while [ ! -S /run/nordvpnd.sock ]; do
 	sleep 0.25
 done
 
-nordvpn login -u ${USER} -p "${PASS}"
+nordvpn login -u "${USER}" -p "${PASS}"
 
 setup_nordvpn
 create_tun_device
@@ -131,4 +132,4 @@ create_tun_device
 nordvpn connect ${CONNECT} || exit 1
 nordvpn status
 
-tail -f --pid=$(pidof nordvpnd) /var/log/nordvpn/daemon.log
+tail -f --pid="$(pidof nordvpnd)" /var/log/nordvpn/daemon.log
